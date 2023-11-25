f813ff60bb1e14789fe6126fb5c40c0042fd3a4b
daniocen777 <danielochoa@outlook.com>
2023-11-25 09:18:23 -0500

    Crud countries

---
+++ Shooping/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shooping.Data.Entities;
+
+namespace Shooping.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        // add-migration [DescriptionCamelCase] => Para la modificacion o creacion de atributos de las tablas
+        // update-database => Creaar la BD
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Crear index para que nombre de pais sea unico
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
