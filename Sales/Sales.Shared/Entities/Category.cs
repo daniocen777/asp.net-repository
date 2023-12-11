c7c751d6d44b49e16cb91bad7788c1619cf37b5b
daniocen777 <danielochoa@outlook.com>
2023-12-11 09:21:34 -0500

    CRUD de categorías terminado

--- Sales/Sales.Shared/Entities/Category.cs
+++ Sales/Sales.Shared/Entities/Category.cs
@@ -7,7 +7,7 @@ namespace Sales.Shared.Entities
         public int Id { get; set; }
 
         [Display(Name = "Categoría")]
-        [Required]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
         public string Name { get; set; } = null!;
     }
--- Sales/Sales.Shared/obj/Debug/net8.0/Sales.Shared.AssemblyInfo.cs
+++ Sales/Sales.Shared/obj/Debug/net8.0/Sales.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sales.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64f57dc59b2cdef83d54d8606c36d936c6c53f15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dc23ffc1db3f7407f20c8ed386d1db479d33512")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sales.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sales.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
